6a55bda237444c8adc2f68fe3f128f47a10f56f9
zhuchentong <2037630@163.com>
2019-12-15 18:38:40 +0800

    udpate

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { User } from 'src/entities/user.entity'
-import { Group } from 'src/entities/group.entity'
 
 @Controller('/auth')
 export class AuthController {
