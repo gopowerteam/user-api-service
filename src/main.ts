134b1b03019a0f0b480038fc3397f20dd394ea6d
Duang(杜昂) <hr837@163.com>
2019-12-19 17:00:28 +0800

    添加swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Application } from './core/application'
 import { APP_CONFIG_PROVIDER } from './core/constants'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 // https
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule).then(Application.initialize)
+
+  const options = new DocumentBuilder()
+    .setTitle('User Api Document')
+    .setDescription('The project power by GoPower team')
+    .setVersion('0.0.1')
+    .addTag('default')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
   // 获取配置服务
   const config = app.get(APP_CONFIG_PROVIDER)
   // 获取监听端口
